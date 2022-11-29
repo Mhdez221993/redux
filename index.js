12964eb762b5055fdfb2354b55f99b55500ce23c
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-29 17:39:56 +0100

    Setup redux & create store and reducer

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log('From index js');
+const redux = require('redux');
+const createStore = redux.createStore
+
+const CAKE_ORDERED = 'CAKE_ORDERED'
+
+const orderCake = () => {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1
+  }
+}
+
+const initaialState = {
+  numOfCakes: 10
+}
+
+const reducer = (state = initaialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1
+      }
+
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
+console.log('Initial state', store.getState());
+
+const unsubscribe = store.subscribe(() => console.log('Update stae', store.getState()))
+
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+
+unsubscribe()
