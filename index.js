9f8f1edb9bda4518356bb89f738de63a64196d6a
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-29 17:46:26 +0100

    Add new action creator & dispach it to test out

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const redux = require('redux');
 const createStore = redux.createStore
 
 const CAKE_ORDERED = 'CAKE_ORDERED'
+const CAKE_RESTOCKED = 'CAKE_RESTOKED'
 
 const orderCake = () => {
   return {
     type: CAKE_ORDERED,
-    quantity: 1
+    payload: 1
+  }
+}
+
+const restokeCake = (payload) => {
+  return {
+    type: CAKE_RESTOCKED,
+    payload
   }
 }
 
@@ -22,6 +30,12 @@ const reducer = (state = initaialState, action) => {
         numOfCakes: state.numOfCakes - 1
       }
 
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload
+      }
+
     default:
       return state
   }
@@ -35,5 +49,6 @@ const unsubscribe = store.subscribe(() => console.log('Update stae', store.getSt
 store.dispatch(orderCake())
 store.dispatch(orderCake())
 store.dispatch(orderCake())
+store.dispatch(restokeCake(3))
 
 unsubscribe()
