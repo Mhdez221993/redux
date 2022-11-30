e3224a24fbaf267a6e18afc41b76c4ba62fd6cda
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-30 09:23:17 +0100

    Import Provider & store on main.js

--- react-toolkit/src/main.jsx
+++ react-toolkit/src/main.jsx
@@ -1,10 +1,15 @@
+import './index.css'
+
+import App from './App'
+import Provider from 'react-redux'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import store from './app/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
