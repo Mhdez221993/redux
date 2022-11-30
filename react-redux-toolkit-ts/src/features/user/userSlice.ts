9fc1b924a48708e57f47e217a06101b498aa4683
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-30 12:09:21 +0100

    Type userSlice & User component

--- react-redux-toolkit-ts/src/features/user/userSlice.ts
+++ react-redux-toolkit-ts/src/features/user/userSlice.ts
@@ -1,10 +1,19 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
 import axios from 'axios'
 
-// import axios from 'axios'
+type User = {
+  id: number
+  name: string
+}
 
-const initialState = {
+type initialState = {
+  isLoading: boolean,
+  users: User[],
+  error: string
+}
+
+const initialState: initialState = {
   isLoading: true,
   users: [],
   error: ''
@@ -12,24 +21,25 @@ const initialState = {
 
 export const fetchUsers = createAsyncThunk('user/fetchUsers', async () => {
   return await axios('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.data)
+    .then((response => response.data))
 })
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
+  reducers: {},
   extraReducers: builder => {
     builder.addCase(fetchUsers.pending, (state) => {
         state.isLoading = true
       })
-    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+    builder.addCase(fetchUsers.fulfilled, (state, action: PayloadAction<User[]>) => {
       state.users = action.payload
       state.isLoading = false
       state.error = ''
     })
     builder.addCase(fetchUsers.rejected, (state, action) => {
       console.log(state);
-      state.error = action.error.message
+      state.error = action.error.message || 'Something went wrong'
       state.users = []
       state.isLoading = false
     })
--- react-toolkit/src/features/user/UserView.jsx
+++ react-toolkit/src/features/user/UserView.jsx
@@ -18,7 +18,7 @@ const UserView = () => {
 
       <ul>
         {
-          users.map(user => <li key={user.id}>{user.name}</li>)
+          users.map(user => <li key={user.id}>{user.name} {' '} {user.email}</li>)
         }
       </ul>
 
