3059192fd0f26ca0bbb111f3525417a1ebe008b3
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-30 10:29:33 +0100

    Dispatch action to store

--- react-toolkit/src/features/icecream/IcecreamView.jsx
+++ react-toolkit/src/features/icecream/IcecreamView.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { ordered, restoked } from './icecreamSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 const IcecreamView = () => {
   const {numOfIcecreams} = useSelector(state => state.icecreams)
+  const [value, setValue] = useState(1)
+  const dispatch = useDispatch()
 
   return (
     <div>
         <h2>Number of ice cream- {numOfIcecreams}</h2>
-        <button>Order ice cream</button>
-        <button>Restock ice cream</button>
+        <button onClick={() => dispatch(ordered())}>Order ice cream</button>
+        <input type="text" value={value} onChange={(e) => setValue(parseInt(e.target.value))}/>
+        <button onClick={() => dispatch(restoked(value))}>Restock ice cream</button>
     </div>
-  );
-};
+  )
+}
 
-export default IcecreamView;
+export default IcecreamView
